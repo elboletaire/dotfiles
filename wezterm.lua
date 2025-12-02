4c4e0310f2d61b9e7fffbc2bfb4b426f5291558f
Òscar Casajuana <elboletaire@gmail.com>
2025-12-02 11:59:12 +0100

    chore(wezterm): config changes

--- wezterm.lua
+++ wezterm.lua
@@ -7,8 +7,11 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices.
 
 -- For example, changing the initial geometry for new windows:
-config.initial_cols = 120
-config.initial_rows = 28
+config.initial_cols = 130
+config.initial_rows = 35
+
+-- Change scrollback
+config.scrollback_lines = 10000
 
 -- or, changing the font size and color scheme.
 config.font_size = 10
@@ -18,6 +21,9 @@ config.window_frame = {
   active_titlebar_bg = "#161517",
   inactive_titlebar_bg = "#363537"
 }
+
+-- open in same window as new tab
+config.prefer_to_spawn_tabs = true
 
 -- Finally, return the configuration to wezterm:
 return config
